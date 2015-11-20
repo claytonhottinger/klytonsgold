363e57d2b7b12c9764e956337105e0b5aea84a39
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2015-11-20 17:01:50 -0600

    what up suckas

--- examples.js
+++ examples.js
@@ -1,40 +1,11 @@
-/**
- * Created by joelmiller on 7/14/15.
- */
-$(document).ready(function() {
+$(function() {
+  var cheesyTemplate = Handlebars.compile($('#colores').html());
 
-    $.ajax('insert.html', {
-        success: function(response){$('.myContainer').html(response)},
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
-
-    $.ajax('data.json', {
-        success: function(response){
-            var msg = $("<p></p>");
-            msg.append("Color " + response.color);
-            msg.append("Qty " + response.value);
-            $('.myContainer').html(msg)
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
-
-    $.ajax('data.json', {
-        success: function(response){
-            $.each(result, function(index, myObj) {
-                var msg = $("<p></p>");
-                msg.append("Price " + myObj.color);
-                msg.append("Qty " + myObj.value);
-                $('.myContainer').html(msg)
-            })
-
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
-
-})
+  $.ajax({
+    url: 'data.json'
+  })
+  .done(function(json) {
+    var compiledHtml = cheesyTemplate({couleur: json});
+    $('body').html(compiledHtml);
+  });
+});
